c8eab3c76cfac07d20e680b0e6149378960b5ca2
ikshita <ikshimadhub@gmail.com>
2022-10-29 01:37:34 +0400

    login page and action

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+    include("connection.php");
+
+    if(isset($_POST['login'])){
+        $username = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "select * from login where username = '$username' and password = '$password'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+        if($count==1){
+            header("Location:welcome.php");
+        }
+        else{
+            echo '<script>
+            window.location.href = "index.php";
+            alert("Login failed. Invalid username or password!")
+            </script>';
+        }
+    }
+    
+?>
---
+++ welcome.php
@@ -0,0 +1,28 @@
+<?php
+  include("connection.php");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css" rel="stylesheet" />
+
+    <!-- MDB -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"></script>
+
+    <link rel="stylesheet" href="login.css">
+    <script type="text/javascript" src="login.js"></script>
+</head>
+<body>
+
+   <h1>Welcome!</h1>
+
+</body>
+</html>
